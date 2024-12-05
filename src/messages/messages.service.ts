3a21ca4536dc59a2a6e2e009c57928bcb2ca1633
Bart Frackiewicz <decomplosed@gmail.com>
2024-12-05 09:55:07 +0100

    Applied methods in the service

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -6,4 +6,16 @@ export class MessagesService {
   constructor() {
     this.messagesRepo = new MessagesRepository();
   }
+
+  findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+
+  findAll() {
+    return this.messagesRepo.findAll();
+  }
+
+  create(content: string) {
+    return this.messagesRepo.create(content);
+  }
 }
