093af4987aec7ba8195791475bdfbbac7165a08b
Bart Frackiewicz <decomplosed@gmail.com>
2024-12-05 09:48:15 +0100

    Setup findAll method in repo

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -7,6 +7,13 @@ export class MessagesRepository {
 
     return messages[id];
   }
-  async findAll() {}
+
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+
+    return messages;
+  }
+
   async create(message: string) {}
 }
